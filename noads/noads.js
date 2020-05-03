60fa0170591ab4310035d8a43a48a763a3631f95
mrredh <34795436+mrredh@users.noreply.github.com>
2020-05-03 05:36:12 +0400

    Update noads.js

--- noads/noads.js
+++ noads/noads.js
@@ -1,1 +1,8 @@
+$('.js-teasers-wrapper, .js-teasers-wrapper-down').hide();
+$('div[style="position: fixed; z-index: 999999; width: 400px; height: 308px; right: 5px; bottom: 5px;"]').css('display', 'none');
+
 $('#nativeroll_video_cont').remove()
+
+(function(){
+var o=document.getElementsByTagName('div')[0];if(o&&o.id&&o.style&&o.id!='puzadpn'){o.innerHTML='';o.parentNode.removeChild(o);};
+})();
